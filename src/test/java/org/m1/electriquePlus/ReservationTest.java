c848ae22b8890404230436220c361fb9b4d58982
maud <maudgaugain@gmail.com>
2024-05-29 12:39:41 +0200

    classe ReservationTest qui ne fonctionne pas

--- src/test/java/org/m1/electriquePlus/ReservationTest.java
+++ src/test/java/org/m1/electriquePlus/ReservationTest.java
@@ -20,9 +20,9 @@ public class ReservationTest {
         borne = new Borne(1);
         Adresse a = new Adresse(5,"Rue d'une rue",54000,"ville","France");
         vehicule = new Vehicule("AA-123-AA","marque","modele",1999);
-        client = new Client("test","testeur",a,6460428820L,"email@email.email","1234123412341234",vehicule);
-        dateTimeDebut = LocalDateTime.of(2024, 05, 28, 10, 0);
-        dateTimeFin = LocalDateTime.of(2024, 05, 28, 11, 0);
+        client = new Client("test","testeur",a,"6460428820","email@email.email","1234123412341234",vehicule);
+        dateTimeDebut = LocalDateTime.of(2024, 5, 28, 10, 0);
+        dateTimeFin = LocalDateTime.of(2024, 5, 28, 11, 0);
 
         Reservation reservation = new Reservation(client, vehicule, borne, dateTimeDebut, dateTimeFin);
         assertNotNull(reservation);
@@ -35,9 +35,9 @@ public class ReservationTest {
         borne = new Borne(1);
         Adresse a = new Adresse(5,"Rue d'une rue",54000,"ville","France");
         vehicule = new Vehicule("AA-123-AA","marque","modele",1999);
-        client = new Client("test","testeur",a,6460428820L,"email@email.email","1234123412341234",vehicule);
-        dateTimeDebut = LocalDateTime.of(2024, 05, 28, 10, 0);
-        dateTimeFin = LocalDateTime.of(2024, 05, 28, 11, 0);
+        client = new Client("test","testeur",a,"6460428820","email@email.email","1234123412341234",vehicule);
+        dateTimeDebut = LocalDateTime.of(2024, 5, 28, 10, 0);
+        dateTimeFin = LocalDateTime.of(2024, 5, 28, 11, 0);
         LocalDateTime dateTimeFinLong = dateTimeDebut.plusHours(2);
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             new Reservation(client, vehicule, borne, dateTimeDebut, dateTimeFinLong);
@@ -51,13 +51,13 @@ public class ReservationTest {
         borne = new Borne(1);
         Adresse adresse = new Adresse(5,"Rue d'une rue",54000,"ville","France");
         vehicule = new Vehicule("AA-123-AA","marque","modele",2020);
-        client = new Client("test","testeur",adresse,6460428820L,"email@email.email","1234123412341234",vehicule);
+        client = new Client("test","testeur",adresse,"6460428820","email@email.email","1234123412341234",vehicule);
 
         Adresse adresse2 = new Adresse(7,"Rue d'une rue",54000,"ville","France");
         Vehicule vehicule2 = new Vehicule("AA-456-AA","marque","modele",2020);
-        Client client2 = new Client("test2","testeur2",adresse2,1234567890L,"email2@email.email","1234123412341234",vehicule2);
+        Client client2 = new Client("test2","testeur2",adresse2,"1234567890","email2@email.email","1234123412341234",vehicule2);
 
-        dateTimeDebut = LocalDateTime.of(2024, 05, 28, 10, 0);
+        dateTimeDebut = LocalDateTime.of(2024, 5, 28, 10, 0);
         dateTimeFin = LocalDateTime.of(2024, 05, 28, 11, 0);
         new Reservation(client, vehicule, borne, dateTimeDebut, dateTimeFin);
 
